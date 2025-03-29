238d19f50f76d74c258032b2916128276105233a
Admin <Admin@DESKTOP-DOS62G3>
2025-03-30 00:42:50 +0900

    Quitar api key

--- src/main/java/com/calculo/demo/CalculadoraController.java
+++ src/main/java/com/calculo/demo/CalculadoraController.java
@@ -1,6 +1,5 @@
 package com.calculo.demo;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,16 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class CalculadoraController {
     
-    private static final String API_KEY = "mi-clave-segura";
 
-    @GetMapping("/sumar")
-    public ResponseEntity<Integer> sumar(
-            @RequestParam int a, @RequestParam int b,
-            @RequestParam String apiKey) {
+	@GetMapping("/sumar")
+	public ResponseEntity<Integer> sumar(@RequestParam int a, @RequestParam int b) {
 
-        if (!API_KEY.equals(apiKey)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-        return ResponseEntity.ok(a + b);
-    }
+		return ResponseEntity.ok(a + b);
+	}
 }
